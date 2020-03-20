cd4cd5270e7fde3fb08e4216400bf605b40aad8e
kuki <bkko@outlook.kr>
2020-03-20 22:27:25 +0900

    style fix: unnecessary border removed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import styled from 'styled-components'
 import './index.css'
 
 const TodoWrapper = styled.div`
-  border: 1px solid lightgrey;
   background: white;
   max-width: 512px;
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
