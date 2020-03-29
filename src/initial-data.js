7e937300193a869a307b4bf69fc18f87e690d230
kuki <bkko@outlook.kr>
2020-03-29 18:02:54 +0900

    bug fix: iphone input zoom disabled // data in japanses

--- src/initial-data.js
+++ src/initial-data.js
@@ -3,10 +3,10 @@ const initialDatas = {
     date: new Date().toLocaleString('ja-jp'),
     index: 5,
     tasks: {
-        'task-1': {id: 'task-1', 'content': 'take out the garbage', 'checked': false, 'timer': ''},
-        'task-2': {id: 'task-2', 'content': 'watch my favorite show', 'checked': false, 'timer': ''},
-        'task-3': {id: 'task-3', 'content': 'charge my phone', 'checked': false, 'timer': ''},
-        'task-4': {id: 'task-4', 'content': 'cook dinner', 'checked': false, 'timer': ''}
+        'task-1': {id: 'task-1', 'content': '部屋を掃除する', 'checked': false, 'timer': ''},
+        'task-2': {id: 'task-2', 'content': '6時まで上野駅', 'checked': false, 'timer': ''},
+        'task-3': {id: 'task-3', 'content': 'スマホ充電する', 'checked': false, 'timer': ''},
+        'task-4': {id: 'task-4', 'content': '晩ご飯準備する', 'checked': false, 'timer': ''}
     },
     column: {
         'id': 'column-1',
